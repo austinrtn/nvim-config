91743d15fca3464084e6dc3ad0b00d1e31cc0ca1
austinrtn <kolowemployment@gmail.com>
2025-05-03 21:06:41 -0400

    sakd;fjsdkajf|

--- lua/plugins/luasnip-snippets.lua
+++ lua/plugins/luasnip-snippets.lua
@@ -1,10 +1,10 @@
+
 -- ~/.config/nvim/lua/plugins/luasnip-snippets.lua
 return {
   "L3MON4D3/LuaSnip",
-  opts = {
-    -- load custom snippets from `~/.config/nvim/lua/snippets`
-    load_ft_func = function(_, ft_paths)
-      table.insert(ft_paths, "~/.config/nvim/lua/snippets")
-    end,
-  },
+  config = function()
+    require("luasnip.loaders.from_lua").lazy_load({
+      paths = { "~/.config/nvim/lua/snippets" },
+    })
+  end,
 }
