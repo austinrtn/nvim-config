2131e909d65d4710ff67e9c2acd51619611a0279
Dogmatic Polack <kolowemployment@gmail.com>
2025-09-30 00:22:15 -0400

    Telescope and more

--- init.lua
+++ init.lua
@@ -1,6 +1,31 @@
+-- Set leader key to space (must be set before plugins load)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
 vim.opt.relativenumber = true
 vim.opt.number = true
 
+-- Diagnostic configuration for better visibility
+vim.diagnostic.config({
+  virtual_text = {
+    prefix = '●',
+    spacing = 4,
+  },
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+  float = {
+    border = 'rounded',
+    source = 'always',
+  },
+})
+
+-- Define diagnostic signs
+vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticSignError' })
+vim.fn.sign_define('DiagnosticSignWarn', { text = '', texthl = 'DiagnosticSignWarn' })
+vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticSignInfo' })
+vim.fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint' })
 
 vim.keymap.set("n", "<A-z><A-m>", ":ZenMode<CR>", { desc = "Toggle ZenMode", silent = true })
 -- Bootstrap lazy.nvim
---
+++ lua/plugins/dap.lua
@@ -0,0 +1,60 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "theHamsta/nvim-dap-virtual-text",
+      "nvim-neotest/nvim-nio",
+    },
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+
+      dapui.setup()
+      require("nvim-dap-virtual-text").setup()
+
+      -- Automatically open/close dap-ui
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+
+      -- GDB/LLDB adapter for Zig (and C/C++)
+      dap.adapters.lldb = {
+        type = "executable",
+        command = "/usr/bin/lldb-dap", -- adjust path if needed
+        name = "lldb",
+      }
+
+      dap.configurations.zig = {
+        {
+          name = "Launch",
+          type = "lldb",
+          request = "launch",
+          program = function()
+            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/zig-out/bin/", "file")
+          end,
+          cwd = "${workspaceFolder}",
+          stopOnEntry = false,
+          args = {},
+        },
+      }
+
+      -- Keybindings
+      vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Continue" })
+      vim.keymap.set("n", "<F10>", dap.step_over, { desc = "Debug: Step Over" })
+      vim.keymap.set("n", "<F11>", dap.step_into, { desc = "Debug: Step Into" })
+      vim.keymap.set("n", "<F12>", dap.step_out, { desc = "Debug: Step Out" })
+      vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
+      vim.keymap.set("n", "<leader>dB", function()
+        dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+      end, { desc = "Debug: Set Conditional Breakpoint" })
+      vim.keymap.set("n", "<leader>du", dapui.toggle, { desc = "Debug: Toggle UI" })
+    end,
+  },
+}
